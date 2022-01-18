f5811ff9b429a04b689beb60cd7625bdc1b20105
fchoi1 <masterfabster2010@hotmail.com>
2022-01-17 23:54:30 -0500

    fixed poty

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const sequalize = require('./config/connection');
+const sequelize = require('./config/connection');
 
 const hbs = exphbs.create({}); // Setup handlebars
 
 const app = express();
-const PORT = process.env.PROT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -23,6 +23,6 @@ app.use(routes);
 
 // connect to mysql server, sync means taking config and connecting to associted database tables, creates on if not found
 // force means to recreated all of the tables on startup if there are changes
-sequalize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening`));
 });
