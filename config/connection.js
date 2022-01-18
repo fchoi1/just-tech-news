f31f43390ef142c35bd00997ccda8c4fd201bd40
fchoi1 <masterfabster2010@hotmail.com>
2022-01-17 23:47:38 -0500

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 // Import Sequelize
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  require('dotenv').config();
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
