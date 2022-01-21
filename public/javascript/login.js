1064bc9de92c416d2ba97ea4b4468e39ff2045e4
fchoi1 <masterfabster2010@hotmail.com>
2022-01-21 01:43:27 -0500

    fix login.js in public

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -15,8 +15,8 @@ async function signupFormHandler(event) {
       }),
       headers: { 'Content-Type': 'application/json' }
     });
-    // Check response
-    response.ok ? console.log('success') : alert(response.statusText);
+    // Check response and redirect if sucess
+    response.ok ? document.location.replace('/') : alert(response.statusText);
   }
 }
 
@@ -35,7 +35,7 @@ async function loginFormHandler(event) {
       headers: { 'Content-Type': 'application/json' }
     });
     // Check response
-    response.ok ? console.log('success') : alert(response.statusText);
+    response.ok ? document.location.replace('/') : alert(response.statusText);
   }
 }
 
